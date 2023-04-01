e443dee3d88a476143b4e325bc1d63c70de652ef
Yuri Dias <yuri.dazevedo@gmail.com>
2023-04-01 12:45:21 -0300

    refac: main

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 import lexer.Lexer;
-import token.TOKEN_TYPE;
-import token.Token;
-
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
     public static void main(String args[]) throws IOException{
-        Lexer lx = new Lexer(new FileReader("test.luna"));
-        Token t = lx.nextToken();
-        while(t != null){
-            System.out.println(t);
-            t = lx.nextToken();
+        Lexer lexer = new Lexer(new FileReader("test.luna"));
+
+        for(var token = lexer.nextToken(); token != null; token = lexer.nextToken()){
+            System.out.println(token);
         }
-        var ta =TOKEN_TYPE.EQ;
-        System.out.println("Total de tokens lidos " + lx.numTokens());
+
+        System.out.println("Total de tokens lidos " + lexer.numTokens());
     }
 }
